fedcec8765ffd4290534bbca8fd5f0406534e184
tanz3 <3222839916@qq.com>
2022-06-29 21:31:44 +0800

    techpack: audio: Import xiaomi changes * From mona-r-oss
    Signed-off-by: tanz3 <3222839916@qq.com>

--- techpack/audio/asoc/msm-pcm-routing-v2.h
+++ techpack/audio/asoc/msm-pcm-routing-v2.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 #ifndef _MSM_PCM_ROUTING_H
 #define _MSM_PCM_ROUTING_H
@@ -663,6 +664,7 @@ enum {
 	EXT_EC_REF_SLIM_1_TX,
 	EXT_EC_REF_PRI_TDM_TX,
 	EXT_EC_REF_SEC_TDM_TX,
+	EXT_EC_REF_TERT_TDM_TX,
 	EXT_EC_REF_SENARY_MI2S_TX,
 };
 
@@ -742,6 +744,7 @@ struct msm_pcm_stream_app_type_cfg {
 	int acdb_dev_id;
 	int sample_rate;
 	uint32_t copp_token;
+	int channel;
 };
 
 /* dai_id: front-end ID,
